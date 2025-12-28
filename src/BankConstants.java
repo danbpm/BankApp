79b8418d28369514db2c8e88fa3af7bae9a9e3d0
Daniil Biryukov <daniel.biryukov@cosmos.msu.ru>
2025-12-28 21:47:53 +0300

    Очистка master ветки

--- src/BankConstants.java
+++
@@ -1,6 +0,0 @@
-public class BankConstants {
-    // Баланс карт при инициализации
-     static final double startBalance = 0;
-     // Кредитный лимит
-     static final double creditLimit = 10000;
-}
